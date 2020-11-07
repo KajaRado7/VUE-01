d5962611785c8a4c2bb80a2f5296075eb75e6655
Kaja Radošević <73023084+KajaRado7@users.noreply.github.com>
2020-11-07 16:15:54 +0100

    added files

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="jumbotron">
+  <h1 class="display-4">Welcome!</h1>
+  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <hr class="my-4">
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Lorem Ipsum</a>
+  </p>
+</div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+
+h1{
+  color: green;
+}
+
+</style>
+
+
---
+++ src/views/Signup.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="about">
+   <h1>This is a <b>sign up</b> page</h1>
+   <div class="container">
+    <div class="row">
+     <div class="col-sm"></div>
+     <div class="col-sm">
+      <form>
+
+       <div class="form-group">
+        <label for="exampleInputName2">Name</label>
+        <input type="text" class="form-control"
+id="exampleInputName2" aria-describedby="emailHelp" placeholder="Enter 
+name" />  
+      </div>
+
+      <div class="form-group">
+       <label for="exampleInputSurname2">Surname</label>
+       <input type="text" class="form-control"
+id="exampleInputSurname2" placeholder="Enter surname" />
+      </div>
+
+      <div class="form-group">
+       <label for="exampleInputEmail2">Email</label>
+       <input type="email" class="form-control"
+id="exampleInputEmail2" placeholder="Enter email" />
+      </div>
+
+      <div class="form-group">
+       <label for="exampleInputSurname2">Username</label>
+       <input type="text" class="form-control"
+id="exampleInputUsername2" placeholder="Enter username" />
+      </div>
+
+      <div class="form-group">
+       <label for="exampleInputPassword2">Password</label>
+       <input type="password" class="form-control"
+id="exampleInputPassword2" placeholder="Enter password" />
+      </div>
+
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+ </div>
+ <div class="col-sm"></div>
+ </div>
+ </div>
+ </div>
+</template>
+
+<style>
+ b{
+     color: green;
+ }
+ .btn{
+     background-color: green;
+ }
+</style>
